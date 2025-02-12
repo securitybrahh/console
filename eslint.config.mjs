4e1bcd6babeae2d7164fc1da9559fc36d267a36e
lberrymage <logan@lberrymage.dev>
2025-02-11 18:22:04 -0700

    Use `@eslint/migrate-config`-generated ESLint config
    This change has no real impact and is only for aesthetic consistency
    across our ESLint projects.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,15 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: import.meta.dirname,
+    baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
     allConfig: js.configs.all
 });
